f07ae5b8d56794f46ad6c616e169194b2f57655e
Timothy Cherney <tjcherney@hotmail.com>
2026-04-13 15:42:47 -0700

    doc planning

--- build.zig
+++ build.zig
@@ -69,4 +69,13 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    //TODO make sure this works, add doc comments to term
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = exe.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&install_docs.step);
 }
